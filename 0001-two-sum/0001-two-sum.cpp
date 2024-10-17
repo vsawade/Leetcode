d4b99b6c0af1e57f26d56c92d55c152814ce389c
vsawade <vinaysawade1@gmail.com>
2024-10-17 13:20:55 -0400

    Update 0001-two-sum.cpp

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,14 +1,14 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        unordered_map<int, int> num_map;  // Hashmap to store number and its index
+        unordered_map<int, int> num_map;  
         for (int i = 0; i < nums.size(); i++) {
             int complement = target - nums[i];
             if (num_map.find(complement) != num_map.end()) {
-                return {num_map[complement], i};  // Return the indices if complement found
+                return {num_map[complement], i};  
             }
-            num_map[nums[i]] = i;  // Store the current number and its index in the hashmap
+            num_map[nums[i]] = i;  
         }
-        return {};  // Return an empty vector if no solution is found (though problem guarantees one solution)
+        return {};  
     }
 };
